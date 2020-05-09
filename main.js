59d7579f0724bf0b7dd7ab787862468a9dc90c53
Jason Clohosey <clohoseyjason@gmail.com>
2020-05-09 14:41:16 -0400

    Random phrase is showing up in DOM as dashes user input box was added

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-alert ("Hang Him!");
+//global variables
+var phrases = [
+  "Gentleman start your engines",
+  "Door bumper clear",
+  "Too fast entering",
+  "Checkered flag",
+  "Dale Earnhardt",
+  "Jimmie Johnson",
+  "Pit road",
+  "NASCAR Cup Series",
+  "Restrictor plate",
+  "Super Speedway",
+];
+
+var log = [];
+//function to generate random phrase
+function getPhrase(randomPhrase) {
+  randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+  console.log("random Phrase", randomPhrase);
+
+  document.getElementById("phrase").innerHTML = randomPhrase.replace(/[a-zA-Z]/g, "-");
+
+
+  // for (let i = 0; i < randomPhrase.length; i++) {
+  //     const lettersInPhrase = randomPhrase[i];
+  //     console.log(dashes);
+  // }
+}
+getPhrase();
