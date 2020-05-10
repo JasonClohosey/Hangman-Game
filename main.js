b6248839d4c85082bc4bc262d5a0fbd4bc69c233
Jason Clohosey <clohoseyjason@gmail.com>
2020-05-09 23:28:52 -0400

    accomplished moving random phrase to random phrase array and then looping through to find matching letters

--- main.js
+++ main.js
@@ -1,42 +1,53 @@
 //global variables
 var phrases = [
-  "Gentleman start your engines",
-  "Door bumper clear",
-  "Too fast entering",
-  "Checkered flag",
-  "Dale Earnhardt",
-  "Jimmie Johnson",
-  "Pit road",
-  "NASCAR Cup Series",
-  "Restrictor plate",
-  "Super Speedway",
+  "GENTLEMAN START YOR ENGINES",
+  "DOOR BUMPER CLEAR",
+  "TOO FAST ENTERING",
+  "CHECKERED FLAG",
+  "DALE EARNHARDT",
+  "JIMIE JOHNSON",
+  "PIT ROAD",
+  "NASCAR CUP SERIES",
+  "RESTRICTOR PLATE",
+  "SUPER SPEEDWAY",
 ];
 
 var log = [];
+var phraseLog = [];
 
-//function to generate random phrase
-function getPhrase(randomPhrase) {
-  randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
   console.log("random Phrase", randomPhrase);
 
   document.getElementById("phrase").innerHTML = randomPhrase.replace(/[a-zA-Z]/g, "-");
 
+  phraseLog.push(randomPhrase);
+  console.log("PHRASE LOG ARRAY", phraseLog);
 
-  // for (let i = 0; i < randomPhrase.length; i++) {
-  //     const lettersInPhrase = randomPhrase[i];
-  //     console.log(dashes);
-  // }
-}
-getPhrase();
 
 document.getElementById("submit").addEventListener("click", function (userChoice){
-    userChoice = document.getElementById("user-choice").value.toLowerCase();
+    userChoice = document.getElementById("user-choice").value.toUpperCase();
+
+function matchLetters() {
+    for (let i = 0; i < randomPhrase.length; i++) {
+        const lettersInPhrase = randomPhrase[i];
+        console.log("Letters in the phrase", lettersInPhrase);
+
+        if (userChoice === lettersInPhrase) {
+            console.log("There's a match");
+        } else {
+            console.log("NOOOOOOOPE");
+        }
+    }
+
+}
+matchLetters();
+
     if (log.indexOf(userChoice) === -1) {
         log.push(userChoice);
         document.getElementById("used-letters").innerHTML = ("Used Letters: " + log);
         document.getElementById("already-picked").innerHTML = "";
-        console.log(userChoice);
-        console.log("?")
+        // console.log(userChoice);
+        console.log("NICE")
     } else {
         document.getElementById("already-picked").innerHTML = "You already picked that letter, please try again";
         // alert ("You already picked that letter")
