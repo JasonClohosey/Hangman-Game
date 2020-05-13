a8c9b4ecfd8014b49059c40fe3d15c87f232acc5
Jason Clohosey <clohoseyjason@gmail.com>
2020-05-12 22:01:39 -0400

    letters running through game and replacing dashes for correct guesses

--- main.js
+++ main.js
@@ -12,50 +12,100 @@ var phrases = [
   "SUPER SPEEDWAY",
 ];
 
-var log = [];
+var log = [ ];
 var phraseLog = [];
+var wordStatus = null
+let maxWrong = 6;
+let mistakes = 0;
 
 randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
   console.log("random Phrase", randomPhrase);
 
   document.getElementById("phrase").innerHTML = randomPhrase.replace(/[a-zA-Z]/g, "-");
 
-  phraseLog.push(randomPhrase);
-  console.log("PHRASE LOG ARRAY", phraseLog);
+//   phraseLog.push(randomPhrase);
+//   console.log("PHRASE LOG ARRAY", phraseLog);
 
 
 document.getElementById("submit").addEventListener("click", function (userChoice){
     userChoice = document.getElementById("user-choice").value.toUpperCase();
+    console.log(userChoice);
 
-function matchLetters() {
-    for (let i = 0; i < randomPhrase.length; i++) {
-        const lettersInPhrase = randomPhrase[i];
-        console.log("Letters in the phrase", lettersInPhrase);
 
-        if (userChoice === lettersInPhrase) {
-            console.log("There's a match");
-        } else {
-            console.log("NOOOOOOOPE");
-        }
-    }
-
-}
-matchLetters();
-
-    if (log.indexOf(userChoice) === -1) {
+    function matchLetters() {
         log.push(userChoice);
-        document.getElementById("used-letters").innerHTML = ("Used Letters: " + log);
-        document.getElementById("already-picked").innerHTML = "";
-        // console.log(userChoice);
-        console.log("NICE")
-    } else {
-        document.getElementById("already-picked").innerHTML = "You already picked that letter, please try again";
-        // alert ("You already picked that letter")
-        console.log("You already picked that letter")
-    };
+        // (log.indexOf(userChoice) === -1)
+        // document.getElementById("already-picked").innerHTML = "";
+            // log.indexOf(userChoice) === -1 ? log.push(userChoice) : null;
+            if (randomPhrase.indexOf(userChoice) >= 0) {
+                console.log("Lets run guessWord and checkIfGameWon" + userChoice, randomPhrase);
+            guessedWord();
+            checkIfGameWon();
+            } else if (randomPhrase.indexOf(userChoice) === -1) {
+                console.log("Lets run checkIfGameLost" + userChoice, randomPhrase);
+            //   mistakes++;
+            //   updateMistakes();
+            checkIfGameLost();
+            //   updateHangmanPicture();
+            }
+        }
+        matchLetters();
+    
+    
+    document.getElementById("used-letters").innerHTML = ("Used Letters: " + log);
     console.log("This is the log array", log);
     document.getElementById("user-choice").value = "";
+    
+    // function usedLetterAlready(){
+    //     if (log.indexOf(userChoice) === -1)
+    // document.getElementById("already-picked").innerHTML = "";
+    // }
+    
+    // usedLetterAlready()
+
+    function checkIfGameWon() {
+        if (wordStatus === log) {
+          document.getElementById("already-picked").innerHTML = 'You Won!!!';
+        }
+      }
+
+      function checkIfGameLost() {
+        if (mistakes === maxWrong) {
+          document.getElementById("phrase").innerHTML = 'The answer was: ' + answer;
+          document.getElementById('keyboard').innerHTML = 'You Lost!!!';
+        }
+      };
+
+    function guessedWord() {
+        wordStatus = randomPhrase.split('').map(letter => (log.indexOf(letter) >= 0 ? letter : " - ")).join('');
+        document.getElementById("phrase").innerHTML = wordStatus;
+        // console.log(guessedWord());
+      };   
+
+
 });
+
+
+
+
+
+
+    // <---original for loop function--->
+    // for (let i = 0; i < randomPhrase.length; i++) {
+    //     const lettersInPhrase = randomPhrase[i];
+    //     console.log("Letters in the phrase", lettersInPhrase);
+
+    //     if (userChoice === lettersInPhrase) {
+    //         console.log("There's a match");
+    //     } 
+    //     else {
+    //         console.log("NOOOOOOOPE");
+    //     }
+    // }
+
+
+
+
 
 
 
